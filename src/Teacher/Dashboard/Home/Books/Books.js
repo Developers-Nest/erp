81012e5363501b3c8ed2c94ab968c59bcd58473e
deepsi1064 <deepsikumari@gmail.com>
2021-07-11 01:16:46 +0530

    Books section updated - issued books

--- src/Teacher/Dashboard/Home/Books/Books.js
+++ src/Teacher/Dashboard/Home/Books/Books.js
@@ -1,19 +1,8 @@
-// import React from 'react';
-// import {View} from 'react-native';
-
-// import {Text} from 'react-native-paper';
-
-// export default function App({navigation}) {
-//   return (
-//     <View>
-//       <Text>Books Screen Teacher</Text>
-//     </View>
-//   );
-// }
-
 import * as React from 'react';
-import {TextInput} from 'react-native-paper';
+import { TextInput } from 'react-native-paper';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
+import Icon from 'react-native-vector-icons/AntDesign';
 import {
   StyleSheet,
   Text,
@@ -24,65 +13,106 @@ import {
   Keyboard,
 } from 'react-native';
 
-import {
-  event,
-  onChange,
-  setValue,
-  target,
-  value,
-} from 'react-native-reanimated';
-import {Searchbar} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/AntDesign';
 
 export default function IssuedBooksScreen1() {
-  const [activeTab, setActiveTab] = React.useState('Due');
+
+  const [showContent, setShowContent] = React.useState('Due')
 
   const [searchQuery, setSearchQuery] = React.useState('');
 
   const onChangeSearch = query => setSearchQuery(query);
-  function switchTab() {
-    if (activeTab === 'Cleared') {
-      setActiveTab('Due');
-    } else {
-      setActiveTab('Cleared');
-    }
+  
+  function Submitted() {
+    const [searchQuery, setSearchQuery] = React.useState('');
+
+    const onChangeSearch = query => setSearchQuery(query);
+
+    return (
+
+
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.section}>
+            <View style={styles.details}>
+              <View style={styles.userinhostels}>
+              <TouchableOpacity style={styles.differentusers}>
+                
+                  <Text> </Text>
+                  <Text style={{ fontSize: 10, color: '#505069',fontFamily:'OpenSans-Regular'  }}>ID:45321440</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.differentusers}>
+                  <Text style={{ fontWeight: 'normal', fontSize: 18, color: '#211C5A',fontFamily:'Poppins-Regular' }}> Title</Text>
+
+                
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.differentusers}>
+                  <Text style={{ fontSize: 12, color: "#58626C",fontFamily:'Poppins-Regular' ,paddingLeft:5}}>Issued: 21May,2021</Text>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 12, color: '#58636D',fontFamily:'Poppins-Regular' }}> Due: 21Sept,2021</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.details}>
+              <View style={styles.userinhostels}>
+              <TouchableOpacity style={styles.differentusers}>
+                  <Text> </Text>
+                  <Text style={{ fontSize: 10, color: '#505069',fontFamily:'OpenSans-Regular' }}>ID:45321440</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.differentusers}>
+                  <Text style={{ fontWeight: 'normal', fontSize: 18, color: '#211C5A',fontFamily:'Poppins-Regular' }}> Title</Text>
+
+                
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.differentusers}>
+                  <Text style={{ fontSize: 12, color: "#58626C",fontFamily:'Poppins-Regular',paddingLeft:5 }}>Issued: 21May,2021</Text>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 12, color: '#58636D',fontFamily:'Poppins-Regular' }}> Due: 21Sept,2021</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    );
   }
+
   function Due() {
     const [searchQuery, setSearchQuery] = React.useState('');
 
     const onChangeSearch = query => setSearchQuery(query);
 
     return (
+      
       <View style={styles.container}>
         <ScrollView>
-          <View style={styles.section}>
+        <View style={styles.section}>
             <View style={styles.details}>
               <View style={styles.userinhostels}>
-                <TouchableOpacity style={styles.differentusers}>
-                  <Text
-                    style={{
-                      fontWeight: 'normal',
-                      fontSize: 18,
-                      color: '#211C5A',
-                    }}>
-                    {' '}
-                    Title
-                  </Text>
+              <TouchableOpacity style={styles.differentusers}>
+                 <Text></Text>
 
-                  <Text style={{fontSize: 10, color: '#505069'}}>
-                    ID:45321440
-                  </Text>
+                  <Text style={{ fontSize: 10 , color: '#505069',fontFamily:'OpenSans-Regular' }}>ID:45321440</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.differentusers}>
+                  <Text style={{ fontWeight: 'normal', fontSize: 18, color: '#211C5A',fontFamily:'Poppins-Regular' }}> Title</Text>
+
                 </TouchableOpacity>
 
                 <TouchableOpacity style={styles.differentusers}>
-                  <Text style={{fontSize: 12, color: '#58626C'}}>
+                  <Text style={{ fontSize: 12, color: '#1F7C17',paddingLeft:5,fontFamily:'Poppins-Regular' }}>
                     Issued: 21May,2021
                   </Text>
-                  <View style={{flexDirection: 'row'}}>
-                    <Text style={{fontSize: 12, color: '#58636D'}}>
-                      {' '}
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 12, color: '#B04305',fontFamily:'Poppins-Regular' }}>
                       Due: 21Sept,2021
                     </Text>
+
                   </View>
                 </TouchableOpacity>
               </View>
@@ -91,31 +121,26 @@ export default function IssuedBooksScreen1() {
           <View style={styles.section}>
             <View style={styles.details}>
               <View style={styles.userinhostels}>
-                <TouchableOpacity style={styles.differentusers}>
-                  <Text
-                    style={{
-                      fontWeight: 'normal',
-                      fontSize: 18,
-                      color: '#211C5A',
-                    }}>
-                    {' '}
-                    Title
-                  </Text>
+              <TouchableOpacity style={styles.differentusers}>
+                 <Text></Text>
 
-                  <Text style={{fontSize: 10, color: '#505069'}}>
-                    ID:45321440
-                  </Text>
+                  <Text style={{ fontSize: 10 , color: '#505069',fontFamily:'OpenSans-Regular' }}>ID:45321440</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.differentusers}>
+                  <Text style={{ fontWeight: 'normal', fontSize: 18, color: '#211C5A',fontFamily:'Poppins-Regular' }}> Title</Text>
+
+                  
                 </TouchableOpacity>
 
                 <TouchableOpacity style={styles.differentusers}>
-                  <Text style={{fontSize: 12, color: '#58626C'}}>
+                  <Text style={{ fontSize: 12, color: '#1F7C17',fontFamily:'Poppins-Regular',paddingLeft:5 }}>
                     Issued: 21May,2021
                   </Text>
-                  <View style={{flexDirection: 'row'}}>
-                    <Text style={{fontSize: 12, color: '#58636D'}}>
-                      {' '}
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontSize: 12, color: '#B04305',fontFamily:'Poppins-Regular' }}>
                       Due: 21Sept,2021
                     </Text>
+
                   </View>
                 </TouchableOpacity>
               </View>
@@ -123,118 +148,28 @@ export default function IssuedBooksScreen1() {
           </View>
         </ScrollView>
       </View>
-    );
-  }
 
-  function Cleared() {
-    const [searchQuery, setSearchQuery] = React.useState('');
-
-    const onChangeSearch = query => setSearchQuery(query);
-
-    return (
-      <View style={styles.container}>
-        <ScrollView>
-          <View style={styles.section}>
-            <View style={styles.details}>
-              <View style={styles.userinhostels}>
-                <TouchableOpacity style={styles.differentusers}>
-                  <Text
-                    style={{
-                      fontWeight: 'normal',
-                      fontSize: 18,
-                      color: '#211C5A',
-                    }}>
-                    {' '}
-                    Title
-                  </Text>
-
-                  <Text style={{fontSize: 10, color: '#505069'}}>
-                    ID:45321440
-                  </Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.differentusers}>
-                  <Text style={{fontSize: 12, color: '#58626C'}}>
-                    Issued: 21May,2021
-                  </Text>
-                  <View style={{flexDirection: 'row'}}>
-                    <Text style={{fontSize: 12, color: '#58636D'}}>
-                      {' '}
-                      Due: 21Sept,2021
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-
-          <View style={styles.section}>
-            <View style={styles.details}>
-              <View style={styles.userinhostels}>
-                <TouchableOpacity style={styles.differentusers}>
-                  <Text
-                    style={{
-                      fontWeight: 'normal',
-                      fontSize: 18,
-                      color: '#211C5A',
-                    }}>
-                    {' '}
-                    Title
-                  </Text>
-
-                  <Text style={{fontSize: 10, color: '#505069'}}>
-                    ID:45321440
-                  </Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.differentusers}>
-                  <Text style={{fontSize: 12, color: '#58626C'}}>
-                    Issued: 21May,2021
-                  </Text>
-                  <View style={{flexDirection: 'row'}}>
-                    <Text style={{fontSize: 12, color: '#58636D'}}>
-                      {' '}
-                      Due: 21Sept,2021
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-        </ScrollView>
-      </View>
     );
   }
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.maincontainer}>
+        {/* <IssuedBooksheader /> */}
+
+        {/* header start */}
         <View style={styles.header}>
-          <TouchableOpacity onPress={() => {}}>
-            <Icon
-              size={24}
-              color="white"
-              name="left"
-              style={{
-                alignSelf: 'center',
-                fontSize: 25,
-                color: 'white',
-                paddingLeft: 20,
-                paddingTop: 20,
-              }}
-            />
-          </TouchableOpacity>
-          <Text
-            style={{
-              fontStyle: 'normal',
-              fontSize: 28,
-              fontWeight: '600',
-              alignSelf: 'center',
-              paddingLeft: 30,
-              color: 'white',
-            }}>
-            Issued Books
-          </Text>
+            <TouchableOpacity onPress={() => { }}>
+                <Icon size={24} color="white" name="left" style={{ alignSelf: 'center', fontSize: 25, color: 'white', paddingLeft: 20, paddingTop: 20 }} />
+            </TouchableOpacity>
+            <Text style={{ fontStyle: 'normal', fontFamily:'NunitoSans-Regular' ,fontSize: 28, fontWeight: '600', alignSelf: 'center', paddingLeft: 30, color: 'white' }}>
+                Issued Books
+            </Text>
+
         </View>
+
+
+
+        {/* header ends */}
 
         <View
           style={{
@@ -243,60 +178,72 @@ export default function IssuedBooksScreen1() {
             marginBottom: 20,
             marginTop: 20,
           }}>
-          {/* open search */}
+
+
+{/* open search */}
           <View
-            style={{
-              marginLeft: 0,
-            }}>
-            <View style={{marginTop: 10, ...styles.card}}>
-              <TextInput
-                right={<TextInput.Icon name="magnify" />}
-                theme={{
-                  colors: {
-                    primary: '#999',
-                    underlineColor: 'transparent',
-                    background: 'white',
-                  },
-                }}
-                placeholder="Enter book name or ID here"
-                outlineColor="transparent"
-                styles={{
-                  margin: 10,
-                  padding: 10,
-                  backgroundColor: 'white',
-                }}
-                mode="outline"
-              />
-            </View>
-          </View>
-          {/* close search */}
+        style={{
+          marginTop: 10,
+          //make search and card in same line
+          marginLeft:5,
+          justifyContent: 'space-between',
+          width:'95%',
+          flexDirection: 'row',
+          ...styles.shadow,
+        }}>
+       
+        <TextInput
+          style={{width: '80%', ...styles.text_input}}
+          placeholder="Enter book name or ID here"
+        />
+        <TouchableOpacity
+          style={{
+            alignSelf: 'center',
+          }}>
+          <FontAwesome5
+          name="search"
+          style={{
+            alignSelf: 'center',
+            fontSize: 21,
+            color: 'black',
+          }}
+        
+          />
+        </TouchableOpacity>
+      </View>
+
+
         </View>
+
+        {/* close search */}
+
+        {/* tabs section open */}
         <View style={styles.switchTabsView}>
           <TouchableOpacity
             style={{
-              borderBottomWidth: activeTab == 'Due' ? 2 : 0,
-              borderBottomColor: '#58636D',
+              borderBottomWidth: showContent == 'Due' ? 1 : 0,
+              borderBottomColor: 'rgba(176, 67, 5, 1)',
               paddingHorizontal: 4,
               justifyContent: 'center',
               alignItems: 'center',
             }}
-            onPress={() => switchTab()}>
-            <Text style={styles.switchText}>Due</Text>
+            onPress={() => setShowContent('Due')}>
+            <Text style={styles.switchTextDue}>Due</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             style={{
-              borderBottomWidth: activeTab == 'Cleared' ? 2 : 0,
+              borderBottomWidth: showContent == 'Submitted' ? 1 : 0,
               borderBottomColor: '#58636D',
               paddingHorizontal: 4,
               justifyContent: 'center',
               alignItems: 'center',
             }}
-            onPress={() => switchTab()}>
-            <Text style={styles.switchText}>Cleared</Text>
+            onPress={() => setShowContent('Submitted')}>
+            <Text style={styles.switchText}>Submitted</Text>
           </TouchableOpacity>
         </View>
-        {activeTab === 'Due' ? <Due /> : <Cleared />}
+        {showContent === 'Due' ? <Due /> : <Submitted />}
       </View>
     </TouchableWithoutFeedback>
   );
@@ -329,12 +276,13 @@ const styles = StyleSheet.create({
   details: {
     display: 'flex',
     flexDirection: 'column',
-    marginTop: 10,
+    marginTop: 4,
     paddingBottom: 10,
     borderBottomColor: '#333',
+
   },
   userinhostels: {
-    marginTop: 10,
+    marginBottom: 10,
   },
   differentusers: {
     flexDirection: 'row',
@@ -343,17 +291,10 @@ const styles = StyleSheet.create({
   },
   userstext: {
     fontSize: 16,
-    paddingVertical: 4,
+    // paddingVertical: 4,
     fontWeight: '300',
   },
-  belowhr: {
-    display: 'flex',
-    flexDirection: 'row',
-    marginTop: 20,
-    justifyContent: 'space-between',
-    paddingBottom: 10,
-    borderBottomColor: '#333',
-  },
+ 
   search: {
     backgroundColor: 'white',
     color: 'black',
@@ -367,18 +308,40 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#58636D',
     paddingHorizontal: 5,
+    fontFamily:'Poppins-SemiBold',
+    fontWeight:'bold',
+    
   },
   maincontainer: {
     paddingTop: 10,
     flex: 1,
     backgroundColor: '#E5E5E5',
   },
-  card: {
+ 
+  switchTextDue: {
+    fontSize: 14,
+    color: '#B04305',
+    paddingHorizontal: 5,
+    fontFamily:'Poppins-SemiBold',
+    fontWeight:'bold',
+  },
+
+  text_input: {
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    // backgroundColor: 'rgba(249, 249, 249, 1)',
+    height: 50,
+    fontSize: 16,
+    minWidth: 171,
+    backgroundColor: 'white',
+  },
+
+
+  shadow: {
     shadowColor: '#999',
     shadowOffset: {width: 0, height: 1},
     shadowOpacity: 0.5,
     shadowRadius: 12,
-    elevation: 5,
     backgroundColor: 'white',
     borderColor: '#ccc',
     borderWidth: 1,
@@ -390,12 +353,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     margin: 0,
     padding: 0,
-    minWidth: '30%',
+    minWidth: 110,
   },
+  
+  
   header: {
     height: 65,
     marginTop: -10,
     backgroundColor: 'rgba(0, 73, 159, 1)',
     flexDirection: 'row',
-  },
+
+},
+
 });
