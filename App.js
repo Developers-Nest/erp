856946670e028869e2349f043e07b05519ae5b8c
shreya <shreyatr@outlook.com>
2021-07-13 15:47:01 +0530

    single login screen

--- App.js
+++ App.js
@@ -1,48 +1,50 @@
 import React from 'react';
 
-import { Button } from 'react-native-paper';
-import { View, StyleSheet, TextInput } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {Button} from 'react-native-paper';
+import {View, StyleSheet, TextInput} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import Teacher from './src/Teacher/Teacher';
-import Student from './src/Student/Student';
-// import Login from './src/Login';
-// import StudentDashboard from './src/Student/Dashboard/Home/Home';
-// import TeacherDashboard from './src/Teacher/Dashboard/Home/Home';
+// import Teacher from './src/Teacher/Teacher';
+// import Student from './src/Student/Student';
+import Login from './src/Login';
+import StudentDashboard from './src/Student/Dashboard/Home/Home';
+import TeacherDashboard from './src/Teacher/Dashboard/Home/Home';
 
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import UserReducer from './src/reducers/userReducer'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import UserReducer from './src/reducers/userReducer';
 
-const store = createStore(UserReducer)
+const store = createStore(UserReducer);
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator headerMode="none">
-          <Stack.Screen name="Login" component={Login} />
+          {/* <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Teacher Login" component={Teacher} />
-          <Stack.Screen name="Student Login" component={Student} />
+          <Stack.Screen name="Student Login" component={Student} /> */}
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Teacher Dashboard" component={TeacherDashboard} />
+          <Stack.Screen name="Student Dashboard" component={StudentDashboard} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
 }
 
-function Login({ navigation }) {
-  return (
-    <View>
-      <Button onPress={() => navigation.navigate('Teacher Login')}>
-        Teacher
-      </Button>
-      <Button onPress={() => navigation.navigate('Student Login')}>
-        Student
-      </Button>
-    </View>
-  );
-}
+// function Login({navigation}) {
+//   return (
+//     <View>
+//       <Button onPress={() => navigation.navigate('Teacher Login')}>
+//         Teacher
+//       </Button>
+//       <Button onPress={() => navigation.navigate('Student Login')}>
+//         Student
+//       </Button>
+//     </View>
+//   );
+// }
--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,178 @@
-import React from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+// import React from 'react';
+// import {View, StyleSheet, TextInput} from 'react-native';
 
+// import LinearGradient from 'react-native-linear-gradient';
+// import {Button, Searchbar, IconButton} from 'react-native-paper';
+
+// import StudentDashboard from './Student/Dashboard/Home/Home';
+// import TeacherDashboard from './Teacher/Dashboard/Home/Home';
+
+// export default function Login({navigation}) {
+//   return (
+//     <View style={styles.container}>
+//       <LinearGradient
+//         colors={['rgba(0, 73, 159, 1)', 'rgba(176, 67, 5, 0.84)']}
+//         style={styles.linearGradient}
+//         start={{x: 0, y: 0}}
+//         end={{x: 1, y: 0}}>
+//         <View style={styles.textinput_search}>
+//           <TextInput
+//             style={styles.text_input1}
+//             placeholder="Institution Code"
+//             mode="outlined"
+//           />
+//           <IconButton
+//             icon="magnify"
+//             color="white"
+//             size={40}
+//             onPress={() => console.log('Pressed')}
+//             style={styles.search_icon}
+//           />
+//         </View>
+
+//         <TextInput
+//           style={styles.text_input}
+//           placeholder="Email or Username"
+//           mode="outlined"
+//         />
+//         <TextInput
+//           style={styles.text_input}
+//           placeholder="Password"
+//           mode="outlined"
+//           secureTextEntry={true}
+//         />
+//         <Button
+//           style={styles.button}
+//           onPress={() => navigation.navigate('Student Dashboard')}
+//           labelStyle={{color: 'white'}}
+//           uppercase={false}
+//           mode="contained">
+//           Login
+//         </Button>
+//       </LinearGradient>
+//     </View>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//   },
+//   linearGradient: {
+//     justifyContent: 'center',
+//     borderRadius: 5,
+//     height: '100%',
+//     width: '100%',
+//     padding: 40,
+//   },
+//   text_input: {
+//     padding: 10,
+//     paddingHorizontal: 20,
+//     marginVertical: 10,
+//     borderRadius: 10,
+//     backgroundColor: 'rgba(249, 249, 249, 1)',
+//     height: 50,
+//     fontSize: 16,
+//     minWidth: 171,
+//   },
+//   text_input1: {
+//     paddingHorizontal: 20,
+//     height: 50,
+//     fontSize: 16,
+//     borderRadius: 10,
+//     backgroundColor: 'rgba(249, 249, 249, 1)',
+//     width: 171,
+//   },
+//   textinput_search: {
+//     justifyContent: 'space-between',
+//     flexDirection: 'row',
+//   },
+//   search_icon: {padding: 0, margin: 0, alignSelf: 'flex-end'},
+//   button: {
+//     backgroundColor: 'rgba(80, 80, 105, 1)',
+//     alignSelf: 'flex-end',
+//     marginTop: 10,
+//     color: 'white',
+//     padding: 7,
+//     paddingHorizontal: 10,
+//     borderRadius: 15,
+//   },
+// });
+
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {Button, Searchbar, IconButton} from 'react-native-paper';
 
-import StudentDashboard from './Student/Dashboard/Home/Home';
-import TeacherDashboard from './Teacher/Dashboard/Home/Home';
+import {useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 
-export default function Login({navigation}) {
+// helpers
+import post from '../src/services/helpers/request/post';
+import get from '../src/services/helpers/request/get';
+import write from '../src/services/localstorage/write';
+import read from '../src/services/localstorage/read';
+
+// redux
+import {USERINFO} from '../src/reducers/actionType';
+
+export default function App({navigation}) {
+  const [institutionCode, setInstitutionCode] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  useEffect(async () => {
+    // check for token from local storage
+    try {
+      let t = await read('token');
+      let r = await read('role');
+
+      // get user information
+      const response = await get('/user', t);
+      console.log('Use Effect ', response);
+      console.log('Role ', r);
+      if (r === 'Teacher') {
+        navigation.navigate('Teacher Dashboard');
+      } else if (role === 'Student') {
+        navigation.navigate('Student Dashboard');
+      }
+    } catch (err) {
+      // token not found
+      // ask user to login
+    }
+  }, []);
+
+  const handleSubmit = async () => {
+    try {
+      const slug = '/user/login';
+
+      // data for post request
+      let data = {
+        username: username,
+        password: password,
+      };
+
+      // post request
+      const response = await post(slug, data);
+      let role = response.userType.name;
+
+      // write token to local storage
+      try {
+        await write('token', response.token);
+        await write('role', role);
+      } catch (err) {
+        console.log('Cannot write to Local Storage');
+      }
+
+      if (role === 'Teacher') {
+        navigation.navigate('Teacher Dashboard');
+      } else if (role === 'Student') {
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <LinearGradient
@@ -20,6 +185,7 @@ export default function Login({navigation}) {
             style={styles.text_input1}
             placeholder="Institution Code"
             mode="outlined"
+            onChangeText={code => setInstitutionCode(code)}
           />
           <IconButton
             icon="magnify"
@@ -34,18 +200,21 @@ export default function Login({navigation}) {
           style={styles.text_input}
           placeholder="Email or Username"
           mode="outlined"
+          onChangeText={username => setUsername(username)}
         />
         <TextInput
           style={styles.text_input}
           placeholder="Password"
           mode="outlined"
           secureTextEntry={true}
+          onChangeText={password => setPassword(password)}
         />
         <Button
           style={styles.button}
-          onPress={() => navigation.navigate('Student Dashboard')}
+          // onPress={() => navigation.navigate('Teacher Dashboard')}
           labelStyle={{color: 'white'}}
           uppercase={false}
+          onPress={handleSubmit}
           mode="contained">
           Login
         </Button>
