077aaaefb64812bee15d59e69bd1b9e80b474b07
devmehta787 <67186094+devmehta787@users.noreply.github.com>
2021-07-16 13:33:29 +0530

    feedback

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src/Teacher/Dashboard/Home/Assignment/AssignmentEdit';
+import App from './App';
 import { name as appName } from './app.json';
 import RNFetchBlob from 'rn-fetch-blob';
 
