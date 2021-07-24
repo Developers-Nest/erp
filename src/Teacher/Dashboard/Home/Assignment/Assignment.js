0643e1a2123bb44a284ca9bc38ad477d27abf1ba
Shivansh Goel <shivanshgoel21@gmail.com>
2021-07-25 00:37:57 +0530

    AssignmentAdd.js: File upload

--- src/Teacher/Dashboard/Home/Assignment/Assignment.js
+++ src/Teacher/Dashboard/Home/Assignment/Assignment.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  ScrollView,
-  ImageBackground,
-  Button,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from 'react-native';
-
-import {
-  event,
-  onChange,
-  setValue,
-  target,
-  value,
-} from 'react-native-reanimated';
-import {Searchbar} from 'react-native-paper';
-// import HeaderAllocatedList from '../shared/headerallocatedlist';
-// import {AntDesign} from '@expo/vector-icons';
-import Icon from 'react-native-vector-icons/Ionicons';
 import AssignmentDue from './AssignmentDue';
 import AssignmentAdd from './AssignmentAdd';
 import AssignmentEdit from './AssignmentEdit';
