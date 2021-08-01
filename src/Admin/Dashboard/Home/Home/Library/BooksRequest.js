4ba2ce89de6f0e009989721365232866fd7f96e2
deepsi1064 <deepsikumari@gmail.com>
2021-08-01 18:48:55 +0530

    Card design books request after api connection

--- src/Admin/Dashboard/Home/Home/Library/BooksRequest.js
+++ src/Admin/Dashboard/Home/Home/Library/BooksRequest.js
@@ -141,8 +141,8 @@ export default function Booksrequest({ navigation }) {
                                 color: '#211C5A',
                                 fontFamily: 'Poppins-Regular',
                                 marginHorizontal: -5,
-                                marginRight: 150,
-                                paddingRight: 50
+                                // marginRight: 100,
+                                paddingRight: 80
                               }}>
                               {' '}
                               {request.title}
