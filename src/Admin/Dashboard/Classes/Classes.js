568dad28d4b8c0f352440c3ca145fb0ca7e66886
Shivansh Goel <shivanshgoel21@gmail.com>
2021-08-01 13:49:09 +0530

    Admin GoLive

--- src/Admin/Dashboard/Classes/Classes.js
+++ src/Admin/Dashboard/Classes/Classes.js
@@ -1,32 +1,7 @@
-import * as React from 'react';
-
-import {TextInput} from 'react-native-paper';
-import Icon5 from 'react-native-vector-icons/AntDesign';
-
-import IconPhysics1 from 'react-native-vector-icons/Entypo';
-import IconPhysics2 from 'react-native-vector-icons/Ionicons';
-import IconEnglish2 from 'react-native-vector-icons/Feather';
-import IconEnglish1 from 'react-native-vector-icons/MaterialCommunityIcons';
-
-import IconBio1 from 'react-native-vector-icons/FontAwesome5';
-import IconBio2 from 'react-native-vector-icons/FontAwesome5';
-
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  ImageBackground,
-  Button,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from 'react-native';
+import React from 'react';
 
 import LecturesScreen from './LecturesScreen';
 import GoLiveScreen from './GoLive';
-
-import Icon from 'react-native-vector-icons/Ionicons';
 
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -40,7 +15,7 @@ export default function App() {
         component={LecturesScreen}
         options={{ headerShown: false }}
       />
-      <Stack.Screen name="GoLive" component={GoLiveScreen} />
+      <Stack.Screen name="GoLive" component={GoLiveScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
