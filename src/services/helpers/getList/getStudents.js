a97af172534df0e4cab9a0b625d1fe830db30a1a
shreya <shreyatr@outlook.com>
2021-08-02 19:23:03 +0530

    getStudents service

---
+++ src/services/helpers/getList/getStudents.js
@@ -0,0 +1,20 @@
+import get from '../request/get';
+import read from '../../localstorage/read';
+
+/**
+ * @param {course} string course id
+ * @returns array of batches
+ */
+
+async function getStudents(course, batch) {
+  let slug = `/student/course?course=${course}&batch=${batch}`;
+  console.log('Students slug ', slug);
+  let token = await read('token');
+  let response = await get(slug, token);
+  console.log('Students ', response);
+  let batchArray = [];
+
+  return response;
+}
+
+export default getStudents;
