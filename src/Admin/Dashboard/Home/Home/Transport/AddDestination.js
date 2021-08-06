15c57fa883bdfb399b6aacd85f49e23c6667300b
deepsi1064 <deepsikumari@gmail.com>
2021-08-06 19:53:25 +0530

    AdddestinationAPI connected(except time picker(stopTime) )

--- src/Admin/Dashboard/Home/Home/Transport/AddDestination.js
+++ src/Admin/Dashboard/Home/Home/Transport/AddDestination.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import { StyleSheet, View, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import {
     Searchbar,
@@ -28,29 +28,122 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 //redux
 import { useSelector } from 'react-redux';
 
+import get from '../../../../../services/helpers/request/get'
+import post from '../../../../../services/helpers/request/post'
+import read from '../../../../../services/localstorage/read'
+
 
 export default function AddDestination({ navigation }) {
     //theming
     const institute = useSelector(state => state.institute);
-    const [isTimePickerVisible, setTimePickerVisibility] = React.useState(false);
-    const [Time, setTime] = React.useState('15:00')
-    let index = 0;
-    const dateMonths = {
-        1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'June', 7: 'July', 8: 'Aug', 9: 'Sept', 10: 'Oct', 11: 'Nov', 12: 'Dec',
+
+
+
+    //modal selector values
+    const [routenos, setroutenos] = useState([]);
+    //for fee type no API given,make the label and keys
+    const [feetypes, setfeetypes] = useState([
+        {label: 'Annual', key: 'Annual'},
+        {label: 'Bi-Annual', key: 'Bi-Annual'},
+        {label: 'Tri-Annual', key: 'Tri-Annual'},
+        {label: 'Quaterly', key: 'Quaterly'},
+        {label: 'Monthly', key: 'Monthly'},
+
+
+    ]);
+    
+    //data to be sent
+    const [routeno, setrouteno] = useState();
+    const [feetype, setfeetype] = useState();
+    //for textinputs
+    const [feeamount, setfeeamount] = useState('');
+    const [drop, setdrop] = useState('');
+    
+    // const [isTimePickerVisible, setTimePickerVisibility] = React.useState(false);
+    // const [Time, setTime] = React.useState('15:00')
+
+    // const showTimePicker = () => {
+    //     setTimePickerVisibility(true);
+    // };
+
+    // const hideTimePicker = () => {
+    //     setTimePickerVisibility(false);
+    // };
+    // const handleConfirm = (Time) => {
+    //     // console.warn("A date has been picked: ", date.toString());
+    //     setTime(Time.getTime() + " " + TimeMonths[Time.getHour() + 1] + ":" + Time.getMinute())
+    //    hideTimePicker();
+    // };
+  
+    // let index = 0;
+    // const dateMonths = {
+    //     1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'June', 7: 'July', 8: 'Aug', 9: 'Sept', 10: 'Oct', 11: 'Nov', 12: 'Dec',
+    // }
+ 
+ 
+    
+
+
+    const [showtimePicker, setShowTimePicker] = useState(false);
+ const [time, setTime] = useState(null);
+ // handle time select
+ let handleSubmit2 = async sd => {
+    showLoadingScreen();
+    await setTime(sd.toString());
+    setShowTimePicker(false);
+    hideLoadingScreen();
+  };
+    
+ //on save button press action
+ let handleSubmit = async () => {
+    try {
+        let slug = '/transport/destinationAndFees';
+        let token = await read('token');
+        let data;
+
+        data = {
+            pickAndDrop:drop,
+            amount:feeamount,
+            //for modal picker
+            
+            feeType:feetype,
+          stopTime:time,
+          route:routeno,
+
+        };
+        console.log('Driver Data ', data);
+        let res = await post(slug, data, token);
+        console.log('Add Driver ', res);
+        alert('Destination added!!');
+    } catch (err) {
+        alert('Cannot Save !!' + err);
+    }
+}
+//on load
+useEffect(async () => {
+    try {
+        let slug = '/transport/route';
+        let token = await read('token');
+        let res = await get(slug, token);
+        let list = [];
+
+        res &&
+            res.map((res) => {
+                list.push({
+                    label: res.code,
+                    key: res._id,
+                })
+            })
+
+        console.log(list);
+        setroutenos(list);
+
+    } catch (err) {
+        alert('Cannot get route code!!');
     }
 
-    const showTimePicker = () => {
-        setTimePickerVisibility(true);
-    };
 
-    const hideDatePicker = () => {
-        setTimePickerVisibility(false);
-    };
-    const handleConfirm = (date) => {
-        // console.warn("A date has been picked: ", date.toString());
-        setTime(Time.getTime() + " " + TimeMonths[Time.getHour() + 1] + ":" + Time.getMinute())
-        hideTimePicker();
-    };
+}, [])
 
 
     return (
@@ -109,35 +202,34 @@ export default function AddDestination({ navigation }) {
 
 
                 <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', paddingBottom: 10 }}>
-                    <View style={styles.Card}>
+                <ModalSelector
+                        data={routenos}
+                        initValue="Route Code"
+                        onChange={option => {
+                            setrouteno(option.key);
+                        }}
+                        style={styles.card}
+                        initValueTextStyle={styles.SelectedValueSmall}
+                        selectTextStyle={styles.SelectedValueSmall}
+                    />
+                   <View style={styles.Card}>
                         <View style={styles.CardContent}>
-                            <TextInput
-                                style={{ ...styles.search_input }}
-                                placeholder="Route Code"
-                            />
-                        </View>
-                    </View>
-                    <View style={styles.Card}>
-                        <View style={styles.CardContent}>
-                            <TouchableOpacity style={[styles.pickdate]} onPress={showTimePicker}>
-                                <TextInput style={{ marginLeft: 0, fontFamily: 'Poppins-Regular' }}
-                                    placeholder={Time}
+                            {/* time picker */}
+              <Button
+                icon="calendar"
+                mode="contained"
+                color="white"
+                style={{margin: 2}}
+                onPress={() => setShowTimePicker(true)}>
+                {time? time.slice(11,19) : 'TIME'}
+              </Button>
 
-                                />
-                                <Feather size={18} color="black" name="calendar"
-                                    style={{
-                                        marginTop: 10,
-                                        marginRight: 0,
-                                    }}
-                                ></Feather>
-                                <DateTimePickerModal
-                                    isVisible={isTimePickerVisible}
-                                    style={styles.pickdate}
-                                    mode='time'
-                                    onConfirm={handleConfirm}
-                                    onCancel={hideDatePicker}
-                                />
-                            </TouchableOpacity>
+              <DateTimePickerModal
+                isVisible={showtimePicker}
+                mode="time"
+                onConfirm={handleSubmit2}
+                onCancel={() => setShowTimePicker(!showtimePicker)}
+              />
                         </View>
                     </View>
                 </View>
@@ -149,26 +241,32 @@ export default function AddDestination({ navigation }) {
 
 
                 <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', paddingBottom: 10 }}>
-                    <View style={styles.Card}>
-                        <View style={styles.CardContent}>
-                            <TextInput
-                                style={{ ...styles.search_input }}
-                                placeholder="Fee Type"
-                            />
-                        </View>
-                    </View>
+                <ModalSelector
+                        data={feetypes}
+                        initValue="Fee Type"
+                        onChange={option => {
+                            setfeetype(option.key);
+                        }}
+                        style={styles.card}
+                        initValueTextStyle={styles.SelectedValueSmall}
+                        selectTextStyle={styles.SelectedValueSmall}
+                    />
                     <View style={styles.Card}>
                         <View style={styles.CardContent}>
                             <TextInput
                                 style={{ ...styles.search_input }}
                                 placeholder="Fees Amount"
+                                placeholderTextColor="grey"
+                                color="black"
+                                keyboardType="numeric"
+                                onChangeText={val => setfeeamount(val)}
                             />
                         </View>
                     </View>
                 </View>
 
                 <View style={{ width: "100%", paddingTop: 10, paddingLeft: '7%', flexDirection: 'row' }}>
-                    <Text style={styles.section_heading3}>Pick Up Address</Text>
+                    <Text style={styles.section_heading3}>Pick Up and Drop</Text>
                 </View>
 
 
@@ -178,6 +276,9 @@ export default function AddDestination({ navigation }) {
                             <TextInput
                                 style={{ ...styles.search_input }}
                                 placeholder="Enter pick-up address"
+                                placeholderTextColor="grey"
+                                color="black"
+                                onChangeText={val => setdrop(val)}
                             />
                         </View>
                     </View>
@@ -185,30 +286,14 @@ export default function AddDestination({ navigation }) {
                 </View>
 
 
-                <View style={{ width: "100%", paddingTop: 10, paddingLeft: '7%', flexDirection: 'row' }}>
-                    <Text style={styles.section_heading3}>Drop Address</Text>
-                </View>
-
-
-                <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', paddingBottom: 10 }}>
-                    <View style={styles.Card3}>
-                        <View style={styles.CardContent}>
-                            <TextInput
-                                style={{ ...styles.search_input }}
-                                placeholder="Enter drop address"
-                            />
-                        </View>
-                    </View>
-
-                </View>
-
+               
                 <View
                     style={{
                         justifyContent: 'center',
                         alignItems: 'center',
                         padding: 20,
                     }}>
-                    <Button style={{ width: 90 }} color="#5177E7" mode="contained" onPress={() => console.log('Pressed')}>
+                    <Button style={{ width: 90 }} color="#5177E7" mode="contained" onPress={handleSubmit}>
                         SAVE
                     </Button>
                 </View>
@@ -237,7 +322,7 @@ const styles = StyleSheet.create({
         fontStyle: 'normal',
         fontWeight: '500',
         fontSize: 18,
-        lineHeight: 30,
+        lineHeight: 40,
         paddingTop: 3,
         color: '#211C5A',
     },
@@ -365,5 +450,28 @@ const styles = StyleSheet.create({
     header: {
         height: 69,
         flexDirection: 'row',
+    },
+    card: {
+        shadowColor: '#999',
+        height: 60,
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.5,
+        shadowRadius: 12,
+        elevation: 5,
+        backgroundColor: 'white',
+        borderColor: '#ccc',
+
+        borderBottomLeftRadius: 12,
+        borderBottomRightRadius: 12,
+        borderTopRightRadius: 12,
+        borderTopLeftRadius: 12,
+        overflow: 'hidden',
+        alignSelf: 'center',
+        // justifyContent: 'center',
+        // alignContent:'center',
+        margin: 0,
+        padding: 0,
+
+        width: '40%',
     },
 });
