ea4af7fa0eab089aa429a3e9c9e0a9dae9f2e2f7
shreya <shreyatr@outlook.com>
2021-08-07 17:53:03 +0530

    student feedback only comment : api connection

--- src/Student/Dashboard/Home/Feedback/Feedback.js
+++ src/Student/Dashboard/Home/Feedback/Feedback.js
@@ -1,116 +1,4 @@
-// import React, {useState} from 'react';
-// import {View, StyleSheet, TouchableOpacity} from 'react-native';
-// import {
-//   Text,
-//   TextInput,
-//   Appbar,
-//   Card,
-//   Button,
-//   Title,
-//   Paragraph,
-// } from 'react-native-paper';
-
-// //selector
-// import ModalSelector from 'react-native-modal-selector';
-
-// //icons
-// import AntDesign from 'react-native-vector-icons/AntDesign';
-// import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-// // redux
-// import {useSelector} from 'react-redux';
-
-// export default function Feedback({navigation}) {
-//   const [type, setType] = useState([]);
-//   const [courses, setCourses] = useState([]);
-//   const [subjects, setSubjects] = useState([]);
-
-//   //theming
-//   const institute = useSelector(state => state.institute);
-
-//   return (
-//     <View style={{backgroundColor: '#E5E5E5'}}>
-//       <View
-//         style={{
-//           backgroundColor: institute ? institute.themeColor : 'black',
-//           ...styles.header,
-//         }}>
-//         <TouchableOpacity
-//           onPress={() => {
-//             navigation.navigate('Home');
-//           }}>
-//           <AntDesign
-//             size={24}
-//             color="white"
-//             name="left"
-//             style={{
-//               alignSelf: 'center',
-//               fontSize: 25,
-//               color: 'white',
-//               paddingLeft: 20,
-//               paddingTop: 20,
-//             }}
-//           />
-//         </TouchableOpacity>
-//         <Text
-//           style={{
-//             fontStyle: 'normal',
-//             fontSize: 28,
-//             fontWeight: '600',
-//             alignSelf: 'center',
-//             paddingLeft: 30,
-//             color: 'white',
-//             fontFamily: 'NunitoSans-Regular',
-//           }}>
-//           Feedback
-//         </Text>
-//       </View>
-//       <View style={{padding: 10}} />
-//       <View style={{paddingHorizontal:20}}>
-
-//       <View style={{backgroundColor: 'white', justifyContent:'space-evenly'}}>
-//         <ModalSelector
-//           data={type}
-//           initValue="Type"
-//           onChange={option => {
-//             getSubjects(option.key);
-//           }}
-//         />
-//       </View>
-//       <View style={{padding: 10}} />
-//       <Card>
-//         <Card.Content>
-//           <Title>Questionnarie</Title>
-//           <View style={{padding: 2}} />
-//           <Paragraph>
-//             This feedback is about the form that was given to you yesterday
-//             dealing with one of the issues of teachers.
-//           </Paragraph>
-//         </Card.Content>
-//       </Card>
-//       <View style={{padding: 10}} />
-//       <Card style={{height: 200}}>
-//         <Card.Content>
-//           <TextInput
-//             placeholder="Write down your feedback question here..... "
-//             onChange={val => setDiscription(val)}
-//             style={{backgroundColor: 'white'}}
-//           />
-//         </Card.Content>
-//       </Card>
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   header: {
-//     height: 69,
-//     flexDirection: 'row',
-//   },
-// });
-
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {
   Text,
@@ -132,15 +20,95 @@ import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIc
 // redux
 import {useSelector} from 'react-redux';
 
+// helpers
+import read from '../../../../services/localstorage/read';
+import get from '../../../../services/helpers/request/get';
+import post from '../../../../services/helpers/request/post';
+import LoadingScreen from '../../../../components/LoadingScreen/LoadingScreen';
+
 export default function Feedback({navigation}) {
-  const [type, setType] = useState([]);
   const [courses, setCourses] = useState([]);
   const [subjects, setSubjects] = useState([]);
 
-  const [feedback, setFeedback] = useState('')
+  const [feedback, setFeedback] = useState('');
+
+  const [loadingScreen, setLoadingScreen, hideLoadingScreen] = LoadingScreen();
 
   //theming
   const institute = useSelector(state => state.institute);
+
+  //modal content
+  const [types, setTypes] = React.useState([]);
+  const [questions, setQuestions] = React.useState([]);
+
+  //selected values
+  const [type, setType] = React.useState([]);
+
+  //userinfo
+  let userInfo = useSelector(state => state.userInfo);
+
+  //on load fetch feedback type
+  useEffect(async () => {
+    setLoadingScreen();
+    try {
+      let slug = `/feedback/type`;
+      let token = await read('token');
+      let response = await get(slug, token);
+      let list = [];
+
+      response.map(type => {
+        type.feedbackfor === 'Student'
+          ? list.push({
+              label: type.feedbacktype,
+              key: type._id,
+            })
+          : null;
+      });
+      setTypes(list);
+    } catch (err) {
+      alert('Cannot get feedback types!!' + err);
+    }
+
+    hideLoadingScreen();
+  }, []);
+
+  let getQuestions = async type => {
+    setLoadingScreen();
+    setType(type);
+    try {
+      let slug = `/feedback/question`;
+      let token = await read('token');
+      let response = await get(slug, token);
+      let list = [];
+      setQuestions(response);
+    } catch (err) {
+      alert('Cannot get your Batches!!');
+    }
+    hideLoadingScreen();
+  };
+
+  let handleSubmit = async () => {
+    setLoadingScreen();
+    try {
+      let slug = `/feedback`;
+      let token = await read('token');
+      let data = {
+        batch: userInfo.batch,
+        comment: feedback,
+        course: userInfo.course,
+        feedbackBy: userInfo._id,
+        feedbacktype: type,
+        questions: [],
+      };
+      console.log('data: ', data);
+      let response = await post(slug, data, token);
+      console.log('response: ', response);
+      alert('feedback created');
+    } catch (err) {
+      alert('Cannot get your Batches!!');
+    }
+    hideLoadingScreen();
+  };
 
   return (
     <View style={{backgroundColor: '#E5E5E5'}}>
@@ -183,10 +151,10 @@ export default function Feedback({navigation}) {
       <View style={{paddingHorizontal: 20}}>
         <View>
           <ModalSelector
-            data={type}
+            data={types}
             initValue="Type"
             onChange={option => {
-              getSubjects(option.key);
+              getQuestions(option.key);
             }}
             style={styles.card_picker}
             initValueTextStyle={styles.SelectedValueSmall}
@@ -215,6 +183,12 @@ export default function Feedback({navigation}) {
             />
           </Card.Content>
         </Card>
+        <Button
+          onPress={handleSubmit}
+          mode="contained"
+          color={institute.themeColor}>
+          submit
+        </Button>
       </View>
     </View>
   );
