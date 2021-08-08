eebd888762d2b1e76363caaabbc860a0922b025f
Shivansh Goel <shivanshgoel21@gmail.com>
2021-08-08 16:30:49 +0530

    helper module for time

--- src/services/helpers/formatter/time.js
+++ src/services/helpers/formatter/time.js
@@ -1,13 +1,16 @@
 function getTime(date) {
-    date = new Date(date)
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
+    try{
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        let strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    } catch(err){
+        return 'Invalid'
+    }
 }
 
 export default getTime
