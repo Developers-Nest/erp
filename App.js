668128364347b0b9fb96e5c20f8b5ba843ab6502
shreya <shreyatr@outlook.com>
2021-08-11 22:15:28 +0530

    logo design login page

--- App.js
+++ App.js
@@ -8,12 +8,11 @@ import {
   Text,
   Image,
   TextInput,
-
 } from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import RNBootSplash from "react-native-bootsplash";
+import RNBootSplash from 'react-native-bootsplash';
 
 // import Teacher from './src/Teacher/Teacher';
 // import Student from './src/Student/Student';
@@ -58,96 +57,85 @@ export default function App() {
 
 function RoleBased_Login({navigation}) {
   return (
-  
     <View style={styles.container}>
-
-      <Image source={require('./assets/bootsplash_logo.png')}
-
-          style={{ marginTop:25, justifyContent: 'center', marginLeft: 70 }}
-      />
-   
-          <View style={styles.fixToText}>
-              <TouchableOpacity style={styles.button1} onPress={() => navigation.replace('Student Teacher Login')}>
-                  <Text style={styles.text1}>User Login</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button}  onPress={() => navigation.replace('Admin Login')} >
-                  <Text style={styles.text}>Admin Login</Text>
-              </TouchableOpacity>
-
-
-      
-
+      <View style={styles.logoStyle}>
+        <Image source={require('./assets/bootsplash_logo.png')} />
       </View>
 
-
-
-
-
-  </View>
-
+      <View style={styles.fixToText}>
+        <TouchableOpacity
+          style={styles.button1}
+          onPress={() => navigation.replace('Student Teacher Login')}>
+          <Text style={styles.text1}>User Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.replace('Admin Login')}>
+          <Text style={styles.text}>Admin Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      alignContent: 'center',
-      justifyContent: 'center',
-      flex:1,
-      backgroundColor:'rgba(249, 249, 249, 1)'
+    alignContent: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'rgba(249, 249, 249, 1)',
   },
 
   button1: {
-
-      marginTop: 0,
-      marginBottom: 0,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#5177E7',
-      alignSelf: 'flex-end',
-      padding: 3,
-      paddingHorizontal: 25,
-      paddingVertical: 2,
-      borderRadius: 4,
-      marginRight: 30,
-      height: 60,
-      borderColor: '#5177E7',
-      borderWidth: 1.5
-
+    marginTop: 0,
+    marginBottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#5177E7',
+    alignSelf: 'flex-end',
+    padding: 3,
+    paddingHorizontal: 25,
+    paddingVertical: 2,
+    borderRadius: 4,
+    marginRight: 30,
+    height: 60,
+    borderColor: '#5177E7',
+    borderWidth: 1.5,
   },
   text1: {
-      fontSize: 18,
-      fontWeight: '500',
-      lineHeight: 21,
-      letterSpacing: 0.25,
-      color: 'white',
-
+    fontSize: 18,
+    fontWeight: '500',
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    color: 'white',
   },
-
-
-
+  logoStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    paddingBottom: 10,
+  },
   fixToText: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignContent: 'center',
-      borderRadius: 8,
-      margin: 40,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderRadius: 8,
+    marginVertical: 10,
   },
   button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      paddingHorizontal: 25,
-      borderRadius: 4,
-      elevation: 3,
-      backgroundColor: '#5177E7',
-
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#5177E7',
   },
   text: {
-      fontSize: 18,
-      lineHeight: 21,
-      fontWeight: '500',
-      letterSpacing: 0.25,
-      color: 'white',
+    fontSize: 18,
+    lineHeight: 21,
+    fontWeight: '500',
+    letterSpacing: 0.25,
+    color: 'white',
   },
-
 });
